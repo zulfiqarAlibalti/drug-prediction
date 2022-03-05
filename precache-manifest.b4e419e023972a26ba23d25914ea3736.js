a321c1784c610f62fd85c6375fab460791d9289a
Zulfiqar-Ali <52883632+zulfiqarAlibalti@users.noreply.github.com>
2022-03-05 23:17:20 +0500

    frontend-deployment-final

---
+++ precache-manifest.b4e419e023972a26ba23d25914ea3736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cdb0d6f3ec9a43230b0",
+    "url": "/static/css/main.3ca43e24.chunk.css"
+  },
+  {
+    "revision": "9cdb0d6f3ec9a43230b0",
+    "url": "/static/js/main.0fde7a2f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f7bf379b057614a5d15",
+    "url": "/static/css/2.39f226d5.chunk.css"
+  },
+  {
+    "revision": "2f7bf379b057614a5d15",
+    "url": "/static/js/2.8dcfd289.chunk.js"
+  },
+  {
+    "revision": "9073d3ce938a2bef7f7dc8969b4cb64a",
+    "url": "/static/media/bg.9073d3ce.jpg"
+  },
+  {
+    "revision": "56f49185191e2eb1f4a5b25a54da475f",
+    "url": "/index.html"
+  }
+];
